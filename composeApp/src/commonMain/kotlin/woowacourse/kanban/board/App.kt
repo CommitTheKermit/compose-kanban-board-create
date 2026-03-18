a82b2eb899af77fec0f4ede86e86f8b3631fbe44
ujeong <qnf323@naver.com>
2026-03-18 09:26:30 +0900

    refactor: ktlint 적용

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -2,7 +2,6 @@ package woowacourse.kanban.board
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import woowacourse.kanban.create.model.TaskCreateState
 import woowacourse.kanban.create.view.TaskCreateDialog
 
 @Composable
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/create/model/TaskStatus.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/create/model/TaskStatus.kt
@@ -3,5 +3,5 @@ package woowacourse.kanban.create.model
 enum class TaskStatus(val statusDescription: String) {
     TO_DO("To Do"),
     IN_PROGRESS("In Progress"),
-    DONE("Done")
+    DONE("Done"),
 }

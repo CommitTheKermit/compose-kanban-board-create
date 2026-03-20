76d32fb514cb5df8a7f84e3053118a9bd7859629
ujeong <qnf323@naver.com>
2026-03-20 14:49:17 +0900

    test: 태스크가 상태에 맞는 컬럼에 분류되어야 한다.

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/BoardStateTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/model/BoardStateTest.kt
@@ -60,4 +60,58 @@ class BoardStateTest {
             result,
         )
     }
+
+    @Test
+    fun `태스크가 상태에 맞는 컬럼에 분류되어야 한다`() {
+        // given : Done 상태 태스크 1개, To Do 상태 태스크 1개, In Progress 상태 태스크 1개가 주어진다
+        val tasks = listOf(
+            KanbanTask(
+                data = BoardData(
+                    title = Title("제목"),
+                    content = "내용",
+                    tags = Tags(),
+                    nickname = Nickname("아오"),
+                ),
+                status = TaskStatus.DONE,
+            ),
+            KanbanTask(
+                data = BoardData(
+                    title = Title("제목"),
+                    content = "내용",
+                    tags = Tags(),
+                    nickname = Nickname("아오"),
+                ),
+                status = TaskStatus.TO_DO,
+            ),
+            KanbanTask(
+                data = BoardData(
+                    title = Title("제목"),
+                    content = "내용",
+                    tags = Tags(),
+                    nickname = Nickname("아오"),
+                ),
+                status = TaskStatus.IN_PROGRESS,
+            ),
+        )
+
+        // when : 컬럼들을 분류하면 TO_DO, IN_PROGRESS, DONE 상태 별로 리스트에 배치되어야 한다.
+        val state = BoardState()
+        tasks.forEach {
+            state.addCard(it)
+        }
+
+        // then : TO_DO, IN_PROGRESS, DONE 카드 리스트 각각 하나씩 존재해야 한다
+        assertEquals(
+            1,
+            state.todoCardList.size,
+        )
+        assertEquals(
+            1,
+            state.inProgressCardList.size,
+        )
+        assertEquals(
+            1,
+            state.doneCardList.size,
+        )
+    }
 }

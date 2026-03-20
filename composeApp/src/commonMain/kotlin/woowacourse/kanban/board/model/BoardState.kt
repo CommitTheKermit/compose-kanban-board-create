e7cafd3fc5996b6795433737f0685bb01a1029d4
ujeong <qnf323@naver.com>
2026-03-20 19:00:51 +0900

    fix: 카드 생성을 해도 추가가 안되는 문제 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/BoardState.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/model/BoardState.kt
@@ -1,11 +1,12 @@
 package woowacourse.kanban.board.model
 
+import androidx.compose.runtime.mutableStateListOf
 import woowacourse.kanban.create.model.TaskStatus
 
 class BoardState {
-    val todoCardList: MutableList<KanbanTask> = mutableListOf()
-    val inProgressCardList: MutableList<KanbanTask> = mutableListOf()
-    val doneCardList: MutableList<KanbanTask> = mutableListOf()
+    val todoCardList: MutableList<KanbanTask> = mutableStateListOf()
+    val inProgressCardList: MutableList<KanbanTask> = mutableStateListOf()
+    val doneCardList: MutableList<KanbanTask> = mutableStateListOf()
 
     fun calculateProgress(tasks: List<KanbanTask>): Double {
         return tasks.filter { it.status == TaskStatus.DONE }.size.toDouble() / tasks.size.toDouble()

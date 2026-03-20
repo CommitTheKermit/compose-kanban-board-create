cc57946f989e61db2d696dcf329785f599731472
ujeong <qnf323@naver.com>
2026-03-20 21:18:55 +0900

    test: 스낵바 테스트 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/BoardUiTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/ui/BoardUiTest.kt
@@ -53,4 +53,23 @@ class BoardUiTest {
         // then : 칸반 보드에서 입력된 카드가 보여야 한다
         onNodeWithText("태스크제목").assertExists()
     }
+
+    @Test
+    fun `태스크 카드가 생성되고 스낵바가 출력되어야 한다`() = runComposeUiTest {
+        // given : 태스크 카드 정상 입력값이 주어진다
+        setContent {
+            KanbanBoard()
+        }
+
+        // when : 생성 다이얼로그에서 정상적인 값을 입력 후 생성 버튼을 누를 때
+        onNodeWithText("새 태스크 생성").performClick()
+        waitForIdle()
+        onNodeWithText("태스크 제목을 입력하세요").performTextInput("태스크제목")
+        waitForIdle()
+        onNodeWithText("생성").performClick()
+        waitForIdle()
+
+        // then : 칸반 보드 하단에 스낵바가 출력되어야 한다
+        onNodeWithText("새로운 태스크가 추가되었습니다.").assertExists()
+    }
 }
